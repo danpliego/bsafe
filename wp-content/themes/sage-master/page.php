b3559ffc629e1aa046544333f56c7457da608d3d
Dan Pliego <danpliego1@MacBook-Pro-de-MacBook.local>
2016-05-04 23:38:21 -0500

    details on web and mobile

--- wp-content/themes/sage-master/page.php
+++ wp-content/themes/sage-master/page.php
@@ -46,18 +46,27 @@
       <div class="row">
         <div class="col-sm-5">
           <h1 class="unmargin-top">
-            El Blindaje
-            <small>Más Confiable</small>
-            </h1>
-          <p>
-            Debido a la situacion que vivimos en nuestro pais, <strong>B-SAFE</strong> se caracteriza por brindarle la mayor seguridad que su familia y seres queridos necesitan, es por eso que nos preocupamos en brindarle la mayor tranquilidad que existe.
-          </p>
-          <p>
-            Nos hemos dado la tarea de entrenar a nuestro equipo en el extranjero para poderle brindar un excelente servicio y materiales de calidad.
-          </p>
-          <p>
-            Es esta filosofia de trabajo que te invitamos a conocer en nuestras oficinas.
-          </p>
+              El Blindaje
+              <small>Más Confiable</small>
+            <a class="toggle-collapse visible-xs" role="button" data-toggle="collapse" href="#collapseAbout" aria-expanded="false" aria-controls="collapseExample">
+              <i class="fa fa-chevron-down closed"></i>
+              <i class="fa fa-chevron-up open"></i>
+            </a>
+          </h1>
+          <div class="collapse" id="collapseAbout">
+            <p>
+              Debido a la situacion que vivimos en nuestro pais, <strong>B-SAFE</strong> se caracteriza por brindarle la mayor seguridad que su familia y seres queridos necesitan, es por eso que nos preocupamos en brindarle la mayor tranquilidad que existe.
+            </p>
+            <p>
+              Nos hemos dado la tarea de entrenar a nuestro equipo en el extranjero para poderle brindar un excelente servicio y materiales de calidad.
+            </p>
+            <p>
+              Es esta filosofia de trabajo que te invitamos a conocer en nuestras oficinas.
+            </p>
+            <div class="img-container visible-xs">
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/car.png">
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -74,28 +83,32 @@
           <small>
             BLINDAJE
           </small>
+          <a class="toggle-collapse visible-xs" role="button" data-toggle="collapse" href="#collapseLevels" aria-expanded="false" aria-controls="collapseLevels">
+            <i class="fa fa-chevron-down closed"></i>
+            <i class="fa fa-chevron-up open"></i>
+          </a>
         </h1>
       </div>
     </div>
-    <div class="section">
-      <div class="container">
-        <div class="row page-title">
-          <div class="col-md-4">
-            <h2 class="unmargin border-right text-center">
-              CADA NIVEL
-              <small>
-                PARA CADA NECESIDAD
-              </small>
-            </h2>
+    <div class="collapse" id="collapseLevels">
+      <div class="section">
+        <div class="container">
+          <div class="row page-title">
+            <div class="col-md-4">
+              <h2 class="unmargin border-right text-center">
+                CADA NIVEL
+                <small>
+                  PARA CADA NECESIDAD
+                </small>
+              </h2>
+            </div>
+            <div class="col-md-8">
+              <p class="unmargin">
+                Debido a la situacion que vivimos en nuestro pais, B-SAFE se caracteriza por brindarle la mayor seguridad que su familia y seres queridos necesitan, es por eso que nos preocupamos en brindarle la mayor tranquilidad que existe.
+              </p>
+            </div>
           </div>
-          <div class="col-md-8">
-            <p class="unmargin">
-              Debido a la situacion que vivimos en nuestro pais, B-SAFE se caracteriza por brindarle la mayor seguridad que su familia y seres queridos necesitan, es por eso que nos preocupamos en brindarle la mayor tranquilidad que existe.
-            </p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-sm-4 text-center level">
+          <div class="col-sm-4 col-xs-6 text-center level">
             <div class="wrapper">
               <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/level-1.png">
               <h4>
@@ -109,7 +122,7 @@
               </a>
             </div>
           </div>
-          <div class="col-sm-4 text-center level">
+          <div class="col-sm-4 col-xs-6 text-center level">
             <div class="wrapper">
               <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/level-2.png">
               <h4>
@@ -123,7 +136,7 @@
               </a>
             </div>
           </div>
-          <div class="col-sm-4 text-center level">
+          <div class="col-sm-4 col-xs-6 text-center level">
             <div class="wrapper">
               <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/level-3.png">
               <h4>
@@ -137,9 +150,7 @@
               </a>
             </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-sm-4 col-sm-offset-2 text-center level">
+          <div class="col-sm-4 col-xs-6 col-sm-offset-2 text-center level">
             <div class="wrapper">
               <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/level-4.png">
               <h4>
@@ -153,7 +164,7 @@
               </a>
             </div>
           </div>
-          <div class="col-sm-4 text-center level">
+          <div class="col-sm-4 col-xs-6 col-xs-offset-3 text-center level">
             <div class="wrapper">
               <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/level-5.png">
               <h4>
@@ -169,12 +180,12 @@
           </div>
         </div>
       </div>
-    </div>
-    <div class="more-info text-center">
-      <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/car.png">
-      <a href="#" class="close-more">
-        <i class="fa fa-times"></i>
-      </a>
+      <div class="more-info text-center">
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/car.png">
+        <a href="#" class="close-more">
+          <i class="fa fa-times"></i>
+        </a>
+      </div>
     </div>
   </div>
 
@@ -184,48 +195,54 @@
         <h1 class="text-center page-title">
           Areas de
           <small>blindaje</small>
+          <a class="toggle-collapse visible-xs" role="button" data-toggle="collapse" href="#collapseAreas" aria-expanded="false" aria-controls="collapseAreas">
+            <i class="fa fa-chevron-down closed"></i>
+            <i class="fa fa-chevron-up open"></i>
+          </a>
         </h1>
-        <div class="row areas-types">
-          <div class="col-sm-5 col-lg-offset-2">
-            <div class="row">
-              <div class="col-sm-6">
-                <h3 class="unmargin-top">Zona Opaca:</h3>
-                <ul class="list-unstyled">
-                  <li><a href="#">Techo</a></li>
-                  <li><a href="#">Coronas</a></li>
-                  <li><a href="#">Postes A,B,C,D</a></li>
-                  <li><a href="#">Marcos</a></li>
-                  <li><a href="#">Puertas</a></li>
-                  <li><a href="#">Costados</a></li>
-                </ul>
-              </div>
-              <div class="col-sm-5">
-                <ul class="list-unstyled">
-                  <li><a href="#">Quinta puerta</a></li>
-                  <li><a href="#">Conchas del saplicadero</a></li>
-                  <li><a href="#">Contra chapas</a></li>
-                  <li><a href="#">Espejo retrovisor</a></li>
-                  <li><a href="#">Faros traseros</a></li>
-                  <li><a href="#">Piso</a></li>
-                  <li><a href="#">Pared de fuego</a></li>
-                </ul>
+        <div class="collapse" id="collapseAreas">        
+          <div class="row areas-types">
+            <div class="col-sm-5 col-lg-offset-2">
+              <div class="row">
+                <div class="col-sm-6">
+                  <h3 class="unmargin-top">Zona Opaca:</h3>
+                  <ul class="list-unstyled">
+                    <li><a href="#">Techo</a></li>
+                    <li><a href="#">Coronas</a></li>
+                    <li><a href="#">Postes A,B,C,D</a></li>
+                    <li><a href="#">Marcos</a></li>
+                    <li><a href="#">Puertas</a></li>
+                    <li><a href="#">Costados</a></li>
+                  </ul>
+                </div>
+                <div class="col-sm-5">
+                  <ul class="list-unstyled">
+                    <li><a href="#">Quinta puerta</a></li>
+                    <li><a href="#">Conchas del saplicadero</a></li>
+                    <li><a href="#">Contra chapas</a></li>
+                    <li><a href="#">Espejo retrovisor</a></li>
+                    <li><a href="#">Faros traseros</a></li>
+                    <li><a href="#">Piso</a></li>
+                    <li><a href="#">Pared de fuego</a></li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col-sm-4">
-            <h3 class="unmargin-top">ZONA TRANSPARENTE:</h3>
-            <ul class="list-unstyled">
-              <li><a href="#">Parabrisas</a></li>
-              <li><a href="#">Medallon / quinta puerta</a></li>
-              <li><a href="#">Puertas delanteras</a></li>
-              <li><a href="#">Puertas traseras</a></li>
-              <li><a href="#">Costados traseros</a></li>
-            </ul>
+            <div class="col-sm-4">
+              <h3 class="unmargin-top">ZONA TRANSPARENTE:</h3>
+              <ul class="list-unstyled">
+                <li><a href="#">Parabrisas</a></li>
+                <li><a href="#">Medallon / quinta puerta</a></li>
+                <li><a href="#">Puertas delanteras</a></li>
+                <li><a href="#">Puertas traseras</a></li>
+                <li><a href="#">Costados traseros</a></li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="section">
+    <div class="section hidden-xs">
       <div class="container">
         <div class="row">
           <div class="col-sm-2 col-sm-offset-2 text-right">
@@ -250,15 +267,21 @@
       <h1 class="unmargin-top">
         Contacto
       </h1>
-      <p class="text-right">
-        <strong>
-          Carr Federal México Toluca 3128
-          <br>Abdías García Soto, 05530
-          <br>Ciudad de México, D.F.
-          <br>TEL: 01 55 5700 0202
-        </strong>
-      </p>
-      <?php echo do_shortcode( '[contact-form-7 id="5" title="Contact form 1"]' ); ?>
+      <a class="toggle-collapse visible-xs" role="button" data-toggle="collapse" href="#collapseContact" aria-expanded="false" aria-controls="collapseContact">
+        <i class="fa fa-chevron-down closed"></i>
+        <i class="fa fa-chevron-up open"></i>
+      </a>
+      <div class="collapse" id="collapseContact">      
+        <p class="text-right">
+          <strong>
+            Carr Federal México Toluca 3128
+            <br>Abdías García Soto, 05530
+            <br>Ciudad de México, D.F.
+            <br>TEL: 01 55 5700 0202
+          </strong>
+        </p>
+        <?php echo do_shortcode( '[contact-form-7 id="5" title="Contact form 1"]' ); ?>
+      </div>
     </div>
   </div>
 
