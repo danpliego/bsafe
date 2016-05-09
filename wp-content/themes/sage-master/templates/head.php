cb5e650419c923bcefb418490a358782018b487c
Daniel Pliego <danielpliego@gmail.com>
2016-05-08 22:36:55 -0500

    disable zoom mobile

--- wp-content/themes/sage-master/templates/head.php
+++ wp-content/themes/sage-master/templates/head.php
@@ -4,6 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href='https://fonts.googleapis.com/css?family=Roboto:300,700' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
   <?php wp_head(); ?>
 </head>
